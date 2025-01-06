a207e94444dfe50a4e8bb390e5bb5f325c87757f
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-06 18:26:45 +0530

    BalanceWidget added
    php artisan make:filament-widget BalanceWidget
    
      What type of widget do you want to create?
      Chart ........................................................................................................................................ 0
      Stats overview ............................................................................................................................... 1
      Table ........................................................................................................................................ 2
      Custom ....................................................................................................................................... 3
    ❯ 3
    
      What is the resource you would like to create this in?
    ❯
    
      Where would you like to create this?
      The [admin] panel ........................................................................................................................ admin
      [App\Livewire] alongside other Livewire components ................................................................................ App\Livewire
    ❯ admin

---
+++ app/Filament/Widgets/BalanceWidget.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class BalanceWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.balance-widget';
+
+    public function getBalanceData(): array
+    {
+        $income = Transaction::where('type', 'income')->sum('amount');
+        $expense = Transaction::where('type', 'expense')->sum('amount');
+        $balance = $income - $expense;
+        return [
+            'income' => $income,
+            'expense' => $expense,
+            'balance' => $balance,
+        ];
+    }
+}
